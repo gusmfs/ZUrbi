ae671dc4fd28543ac71b42850985797611ae8e35
Gustavo Medeiros <oigostavo@gmail.com>
2026-05-09 17:30:54 -0300

    teste

---
+++ frontend/src/pages/AnaliseDesempenho.jsx
@@ -0,0 +1,14 @@
+import Footer from "../components/Footer";
+
+export default function AnaliseDesempenho() {
+
+  return (
+    <>
+        <div>
+        <h1>Analise de Desempenho</h1>
+        </div>
+        <Footer />
+    
+    </>
+  );
+}
--- zurbi-backend/target/classes/db/migration/V2__seed_data.sql
+++
@@ -1,3 +0,0 @@
--- src/main/resources/db/migration/V2__seed_data.sql
-
--- Seed reservado para dados iniciais– 
